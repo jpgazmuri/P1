149c483f8bef59a4e7d23728360b371982bfcf80
ROSARIO GONZALEZ <rdgonzalez1@uc.cl>
2023-10-29 22:29:46 -0300

    cambios conection.c

--- server/src/sockets/conection.c
+++ server/src/sockets/conection.c
@@ -1,5 +1,7 @@
 #include "conection.h"
 
+int connected_clients = 0; 
+
 //LINKS REFERENCIAS:
 //https://www.man7.org/linux/man-pages/man2/socket.2.html
 //https://man7.org/linux/man-pages/man7/socket.7.html
@@ -44,17 +46,53 @@ PlayersInfo * prepare_sockets_and_get_clients(char * IP, int port){
   PlayersInfo * sockets_clients = malloc(sizeof(PlayersInfo));
 
   // Se aceptan a los primeros 2 clientes que lleguen. "accept" retorna el n° de otro socket asignado para la comunicación
-  printf("Esperando a que se conecte el primer cliente...\n");
-  sockets_clients->socket_c1 = accept(server_socket, (struct sockaddr *)&client1_addr, &addr_size);
-  printf("Primer cliente conectado!\n\n");
+    printf("Esperando a que se conecte el primer cliente...\n");
+    int new_client_socket = accept(server_socket, (struct sockaddr *)&client1_addr, &addr_size);
+
+    if (connected_clients >= 4) {
+        printf("Se alcanzó el límite de clientes conectados. Rechazando nueva conexión.\n");
+        // Aquí puedes notificar al cliente que se ha alcanzado el límite y cerrar la conexión.
+        // También puedes agregar lógica adicional según tus requisitos.
+        // Por ejemplo:
+        // send(new_client_socket, "Límite de clientes alcanzado. Intente más tarde.\n", strlen("Límite de clientes alcanzado. Intente más tarde.\n"), 0);
+        close(new_client_socket);  // Cierra la conexión del cliente
+    } else {
+    // Si no se ha alcanzado el límite de clientes, continúa con el proceso normalmente.
+    // Agrega el nuevo socket del cliente a tu estructura de datos y realiza otras configuraciones necesarias.
+    sockets_clients->socket_c1 = new_client_socket;
+
+    // Incrementa el contador de clientes conectados
+    connected_clients++;
+
+    // Le enviamos al primer cliente un mensaje de bienvenida
+    char * welcome = "Bienvenido Cliente 1!!";
+    server_send_message(sockets_clients->socket_c1, 0, welcome);
+
+    printf("Primer cliente conectado! (Clientes conectados: %d)\n\n", connected_clients);
+}
 
   // Le enviamos al primer cliente un mensaje de bienvenida
   char * welcome = "Bienvenido Cliente 1!!";
   server_send_message(sockets_clients->socket_c1, 0, welcome);
 
   printf("Esperando a que se conecte el segundo cliente...\n");
-  sockets_clients->socket_c2 = accept(server_socket, (struct sockaddr *)&client2_addr, &addr_size);
-  printf("Segundo cliente conectado!\n\n");
+  int new_client_socket2 = accept(server_socket, (struct sockaddr *)&client2_addr, &addr_size);
+
+  if (connected_clients >= 4) {
+      printf("Se alcanzó el límite de clientes conectados. Rechazando nueva conexión.\n");
+      // Notificar y cerrar la conexión del cliente
+      close(new_client_socket2);
+  } else {
+    // Continuar con el proceso normalmente
+    sockets_clients->socket_c2 = new_client_socket2;
+    connected_clients++;
+
+    char * welcome2 = "Bienvenido Cliente 2!!";
+    server_send_message(sockets_clients->socket_c2, 0, welcome2);
+
+    printf("Segundo cliente conectado! (Clientes conectados: %d)\n\n", connected_clients);
+  }
+
 
   // Le enviamos al segundo cliente un mensaje de bienvenida
   welcome = "Bienvenido Cliente 2!!";
